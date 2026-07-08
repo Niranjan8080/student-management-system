108890ebf51d460697d47af664597d0dce6407d6
Niranjan Barbole <barboleniranjan0@gmail.com>
2026-07-08 09:27:04 +0530

    Updated db.py for Render PostgreSQL

--- Backend/db.py
+++ Backend/db.py
@@ -1,11 +1,14 @@
 import psycopg2
 
+import os
+import psycopg2
+
 conn = psycopg2.connect(
-    host="localhost",
-    database="Student_Data",
-    user="postgres",
-    password="Barbole@8080",
-    port="5432"
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
 )
 
 cursor = conn.cursor()
